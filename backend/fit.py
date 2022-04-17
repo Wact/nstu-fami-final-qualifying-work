0ad7fafecbebbea9d787b2aedec407c120a6c780
Artem Malygin <malygin.wm@gmail.com>
2022-04-17 22:26:42 +0700

    Кросс-валидация для модели catboost упразднена. Добавлено разделение исходной выборки на тренировочную и тестовую. Добавлено вычисление метрики r2 по итогу обучения моделей и вывод результата в лог

--- backend/fit.py
+++ backend/fit.py
@@ -3,8 +3,10 @@ import logging
 import os
 import pickle
 
-from catboost import CatBoostRegressor, Pool, cv
+import pandas as pd
+from catboost import CatBoostRegressor, Pool
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 from constants import Constants
 from utils import read_samples, valid_algorithm
@@ -13,14 +15,23 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def get_model_path(algorithm: str, size: int) -> [str, str]:
-    model_filename = f'{algorithm}, samples={size}.cbm'
+def evaluate_and_save_model(model, x_test: pd.DataFrame, y_test: pd.Series, algorithm: str, size: int):
+    # вычисление метрики R^2
+    r2 = model.score(x_test, y_test)
+
+    logger.info(f'The R^2 score of {algorithm} model is {r2}.')
+
+    if not os.path.isdir(Constants.MODELS_DIR):
+        os.mkdir(Constants.MODELS_DIR)
+
+    model_filename = f'{algorithm}, samples={size}, r2={r2:.3f}.cbm'
     model_path = os.path.join(Constants.MODELS_DIR, model_filename)
 
-    return model_filename, model_path
+    if algorithm == 'catboost':
+        model.save_model(model_path)
+    elif algorithm == 'linear':
+        pickle.dump(model, open(model_path, 'wb'))
 
-
-def print_model_name(model_filename: str) -> None:
     logger.info(f'Model filename is "{model_filename}".')
 
 
@@ -45,10 +56,11 @@ def fit() -> None:
     # рассматриваемые признаки
     feature_columns = ['wdc_23']
 
-    if algorithm == 'catboost':
-        # разделение данных на признаки и отклик
-        train_pool = Pool(df[feature_columns], df['target'])
+    # разделение выборки на тренировочную и тестовую
+    x_train, x_test, y_train, y_test = train_test_split(df[feature_columns], df['target'], test_size=0.2,
+                                                        random_state=42)
 
+    if algorithm == 'catboost':
         # параметры обучаемой модели
         params = {
             'iterations': 200,
@@ -57,54 +69,22 @@ def fit() -> None:
             'verbose': False,
         }
 
-        # результаты кросс-валидации
-        scores = cv(train_pool, params, type='TimeSeries')
-
-        # создание папки для сохранения результатов кросс-валидации
-        if not os.path.isdir(Constants.SCORES_DIR):
-            os.mkdir(Constants.SCORES_DIR)
-
-        # имя файла с результатами кросс-валидации
-        scores_filename = f'cb_reg, samples={size}.txt'
-
-        # путь до файла с результатами кросс-валидации
-        scores_path = os.path.join(Constants.SCORES_DIR, scores_filename)
-
-        # запись результатов кросс-валидации в файл
-        with open(scores_path, 'w') as file:
-            print(scores, file=file)
-
         # инициализация модели
         model = CatBoostRegressor(**params)
 
         # обучение модели
-        model.fit(train_pool)
+        model.fit(x_train, y_train, eval_set=Pool(x_test, y_test))
 
         # сохранение модели
-        Constants.MODELS_DIR = 'models'
-        if not os.path.isdir(Constants.MODELS_DIR):
-            os.mkdir(Constants.MODELS_DIR)
-
-        model_filename, model_path = get_model_path(algorithm, size)
-
-        model.save_model(model_path)
-
-        print_model_name(model_filename)
+        evaluate_and_save_model(model, x_test, y_test, algorithm, size)
 
     elif algorithm == 'linear':
         # обучение модели
-        model = LinearRegression().fit(df[feature_columns], df['target'])
+        model = LinearRegression()
+        model.fit(x_train, y_train)
 
         # сохранение модели
-        Constants.MODELS_DIR = 'models'
-        if not os.path.isdir(Constants.MODELS_DIR):
-            os.mkdir(Constants.MODELS_DIR)
-
-        model_filename, model_path = get_model_path(algorithm, size)
-
-        pickle.dump(model, open(model_path, 'wb'))
-
-        print_model_name(model_filename)
+        evaluate_and_save_model(model, x_test, y_test, algorithm, size)
 
 
 if __name__ == '__main__':
